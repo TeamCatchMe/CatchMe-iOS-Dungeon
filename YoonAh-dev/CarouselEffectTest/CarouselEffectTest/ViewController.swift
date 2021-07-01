7fc7884107620de22be48e972fa3ed0b8f94b81c
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-07-01 21:52:11 +0900

    [Add] CarouselLayout Test 추가(#4)

---
+++ YoonAh-dev/CarouselEffectTest/CarouselEffectTest/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  CarouselEffectTest
+//
+//  Created by SHIN YOON AH on 2021/07/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: CarouselLayout())
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+        setupLayout()
+    }
+
+    private func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        let nib = UINib(nibName: testCVC.identifier, bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: testCVC.identifier)
+    }
+    
+    private func setupLayout() {
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: testCVC.identifier, for: indexPath) as? testCVC else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+}
---
+++ YoonAh-dev/CarouselEffectTest/CarouselEffectTest/testCVC.swift
@@ -0,0 +1,18 @@
+//
+//  testCVC.swift
+//  CarouselEffectTest
+//
+//  Created by SHIN YOON AH on 2021/07/01.
+//
+
+import UIKit
+
+class testCVC: UICollectionViewCell {
+    static let identifier = "testCVC"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
