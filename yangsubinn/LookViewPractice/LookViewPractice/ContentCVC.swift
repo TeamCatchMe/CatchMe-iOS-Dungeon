d327b1b6e10dd31984f104628fbd2556743f9018
yangsubinn <cindy010670@gmail.com>
2021-09-06 22:15:41 +0900

    [Feat] LookView Test 1/2 완료

---
+++ yangsubinn/LookViewPractice/LookViewPractice/ContentCVC.swift
@@ -0,0 +1,181 @@
+//
+//  ContentCVC.swift
+//  LookViewPractice
+//
+//  Created by 양수빈 on 2021/09/06.
+//
+
+import UIKit
+import SnapKit
+
+class ContentCVC: UICollectionViewCell {
+    static let identifier = "ContentCVC"
+    
+    let charBackground = UIView()
+    let charImage = UIImageView()
+    let nameLabel = UILabel()
+    let timeLabel = UILabel()
+    let charNameLabel = UILabel()
+    let contentLabel = UILabel()
+    let likeButton = UIButton()
+    let likeCountLabel = UILabel()
+    let contentImage = UIImageView()
+    
+    var hasContentImage: Bool!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configUI()
+        
+        /// Snapkit 설치하기 귀찮아서 하다가 큰 코 다치는 중인 상황
+        /// 결국 snapkit을 설치한 상황
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if hasContentImage == false {
+            noImageSetupLayout()
+        } else {
+            imageSetupLayout()
+        }
+    }
+    
+    func configUI() {
+        charBackground.backgroundColor = .black
+        charBackground.layer.cornerRadius = 50
+        
+        charImage.backgroundColor = .systemPink
+        charImage.layer.cornerRadius = 20
+        
+        nameLabel.text = "냠냐리냠ㅁ"
+        timeLabel.text = "5분 전"
+        
+        charNameLabel.text = "뭘봐요불만있어여?"
+        contentLabel.text = "오늘 떡볶이를 먹었는데 정말 맛있었다. 떡볶이는 세상에서 제일 맛있는 음식이다. 인정."
+        /// contentImage
+        
+        likeButton.backgroundColor = .gray
+        likeCountLabel.text = "15"
+    }
+    
+    func imageSetupLayout() {
+        addSubview(charBackground)
+        addSubview(charImage)
+        addSubview(nameLabel)
+        addSubview(timeLabel)
+        addSubview(charNameLabel)
+        addSubview(contentLabel)
+        addSubview(contentImage)
+        addSubview(likeButton)
+        addSubview(likeCountLabel)
+        
+        charBackground.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview().offset(18)
+            make.width.height.equalTo(50)
+        }
+        
+        charImage.snp.makeConstraints { make in
+            make.center.equalTo(charBackground.snp.center)
+            make.width.height.equalTo(46)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(24)
+            make.leading.equalTo(charBackground.snp.trailing).offset(12)
+        }
+        
+        timeLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(6)
+            make.leading.equalTo(charBackground.snp.trailing).offset(12)
+        }
+        
+        charNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(charBackground.snp.bottom).offset(21)
+            make.leading.trailing.equalToSuperview().offset(18)
+        }
+        
+        contentLabel.snp.makeConstraints { make in
+            make.top.equalTo(charNameLabel.snp.bottom).offset(10)
+            make.leading.trailing.equalToSuperview().inset(18)
+            make.height.equalTo(36)
+        }
+        
+        // image
+        contentImage.snp.makeConstraints { make in
+            make.top.equalTo(contentLabel.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(18)
+            make.height.equalTo(164)
+        }
+        
+        likeButton.snp.makeConstraints { make in
+            make.top.equalTo(contentImage.snp.bottom).offset(15)
+            make.leading.equalToSuperview().offset(18)
+            make.bottom.equalToSuperview().offset(17)
+        }
+        
+        likeCountLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentImage.snp.bottom).offset(16)
+            make.leading.equalTo(likeButton.snp.trailing).offset(2)
+            make.bottom.equalToSuperview().offset(18)
+        }
+    }
+    
+    func noImageSetupLayout() {
+        
+        addSubview(charBackground)
+        addSubview(charImage)
+        addSubview(nameLabel)
+        addSubview(timeLabel)
+        addSubview(charNameLabel)
+        addSubview(contentLabel)
+//        addSubview(contentImage)
+        addSubview(likeButton)
+        addSubview(likeCountLabel)
+        
+        charBackground.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview().offset(18)
+            make.width.height.equalTo(50)
+        }
+        
+        charImage.snp.makeConstraints { make in
+            make.center.equalTo(charBackground.snp.center)
+            make.width.height.equalTo(46)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(24)
+            make.leading.equalTo(charBackground.snp.trailing).offset(12)
+        }
+        
+        timeLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(6)
+            make.leading.equalTo(charBackground.snp.trailing).offset(12)
+        }
+        
+        charNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(charBackground.snp.bottom).offset(21)
+            make.leading.trailing.equalToSuperview().offset(18)
+        }
+        
+        contentLabel.snp.makeConstraints { make in
+            make.top.equalTo(charNameLabel.snp.bottom).offset(10)
+            make.leading.trailing.equalToSuperview().inset(18)
+            make.height.equalTo(36)
+        }
+        
+        likeButton.snp.makeConstraints { make in
+            make.top.equalTo(contentLabel.snp.bottom).offset(15)
+            make.leading.equalToSuperview().offset(18)
+        }
+        
+        likeCountLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentLabel.snp.bottom).offset(16)
+            make.leading.equalTo(likeButton.snp.trailing).offset(2)
+        }
+    }
+}
---
+++ yangsubinn/LookViewPractice/LookViewPractice/ViewController.swift
@@ -0,0 +1,162 @@
+
+//  ViewController.swift
+//  LookViewPractice
+//
+//  Created by 양수빈 on 2021/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let categories = ["News", "Popular", "Popular"]
+    let contentImages = [nil, ".purple", ".yellow", nil, nil]
+
+    lazy var categoriesCollectionView: UICollectionView = {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 4, left: 29, bottom: 4, right: 29)
+        layout.itemSize = CGSize(width: (UIScreen.main.bounds.width - 29*2) / 3 - 3, height: 29)
+        layout.minimumInteritemSpacing = 1
+        
+        let collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView.register(categoriesCVC.self, forCellWithReuseIdentifier: "categoriesCVC")
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isScrollEnabled = false
+        collectionView.backgroundColor = .clear
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        return collectionView
+    }()
+    
+    lazy var contentCollectionView: UICollectionView = {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 20, bottom: 100, right: 20)
+        
+        let estimateHeight: CGFloat = 390
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width - 40, height: estimateHeight)
+        
+        layout.minimumInteritemSpacing = 26
+       
+        let colletionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+        colletionView.register(ContentCVC.self, forCellWithReuseIdentifier: "ContentCVC")
+        colletionView.backgroundColor = .clear
+        colletionView.isScrollEnabled = true
+        colletionView.showsVerticalScrollIndicator = true
+        
+        colletionView.dataSource = self
+        colletionView.delegate = self
+        
+        return colletionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(categoriesCollectionView)
+        view.addSubview(contentCollectionView)
+        
+        categoriesCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        categoriesCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        categoriesCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        categoriesCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        categoriesCollectionView.heightAnchor.constraint(equalToConstant: 32).isActive = true
+        
+        contentCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(categoriesCollectionView.snp.bottom).offset(2)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalToSuperview()
+        }
+    }
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch collectionView {
+        case categoriesCollectionView:
+            return categories.count
+            
+        case contentCollectionView:
+            return contentImages.count
+        default:
+            return 5
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch collectionView {
+        case categoriesCollectionView:
+            guard let categoriesCell = collectionView.dequeueReusableCell(withReuseIdentifier: categoriesCVC.identifier, for: indexPath) as? categoriesCVC else { return UICollectionViewCell() }
+            
+            categoriesCell.textLabel.text = categories[indexPath.row]
+            categoriesCell.layer.cornerRadius = 15
+            categoriesCell.backgroundColor = .clear
+            categoriesCell.textLabel.textColor = .lightGray
+            
+            if indexPath.row == 0 {
+                collectionView.selectItem(at: indexPath, animated: false, scrollPosition: .init())
+                categoriesCell.isSelected = true
+            }
+            
+            return categoriesCell
+            
+        case contentCollectionView:
+            guard let contentCell = collectionView.dequeueReusableCell(withReuseIdentifier: ContentCVC.identifier, for: indexPath) as? ContentCVC else { return UICollectionViewCell() }
+            
+            contentCell.backgroundColor = .brown
+            contentCell.layer.cornerRadius = 20
+            
+            print("contentImages[\(indexPath.item)]: \(contentImages[indexPath.item])")
+            print("-----------------")
+            
+            if contentImages[indexPath.item] == nil {
+                contentCell.hasContentImage = false
+                contentCell.charNameLabel.text = "이미지X"
+            } else {
+                contentCell.hasContentImage = true
+                contentCell.contentImage.backgroundColor = .white
+                contentCell.charNameLabel.text = "이미지O"
+            }
+            
+//            let width = UIScreen.main.bounds.width
+//            let estimateHeight: CGFloat = 390
+//
+//            contentCell.systemLayoutSizeFitting(CGSize(width: width, height: estimateHeight))
+            
+//            layout.itemSize = CGSize(width: UIScreen.main.bounds.width - 40, height: estimateHeight)
+            
+            return contentCell
+            
+        default:
+            return UICollectionViewCell()
+        }
+    }
+    
+//    func collectionview(_ collectionView: UICollectionView, sizeForItemAt indexPath: IndexPath) -> CGSize {
+////        if collectionView == contentCollectionView {
+//////            guard let contentCell = collectionView.dequeueReusableCell(withReuseIdentifier: ContentCVC.identifier, for: indexPath) as? ContentCVC else { return UICollectionViewCell() }
+////
+////            let width = UIScreen.main.bounds.width - 40
+////            let estimateHeight: CGFloat = 390
+////
+////            return CGSize(width: width, height: estimateHeight)
+////        } else { return CGSize() }
+//    }
+    
+    /// didSelectItemAt이랑 didDeselectedItemAt으로 하려다가 맨 처음에 기본으로 News가 설정되어있는 상태를 하려니까 막혀서,, categoriesCVC에 isSelected를 override해서 했슴다
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
