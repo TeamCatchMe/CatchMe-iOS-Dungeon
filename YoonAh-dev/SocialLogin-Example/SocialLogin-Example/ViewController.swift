5c0c902ddd598bfd3e5fd2ffa4cfe96ec7a8e1a9
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-09-10 11:18:08 +0900

    [Add] Social Login 프로젝트 생성

---
+++ YoonAh-dev/SocialLogin-Example/SocialLogin-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialLogin-Example
+//
+//  Created by SHIN YOON AH on 2021/09/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
